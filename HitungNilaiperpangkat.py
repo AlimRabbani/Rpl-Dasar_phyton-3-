6d35a5c18ca2a3f557e53347d929e0d973426206
AlimRabbani <alimmanurung18@gmail.com>
2023-01-11 18:41:58 +0800

    Hitung nilai pangkat

---
+++ HitungNilaiperpangkat.py
@@ -0,0 +1,21 @@
+# menghitung nlai perpangkatan  
+
+nilai = int(input("Masukkan angka = ")) 
+pangkat = int(input("Masukkan pangkat = ")) 
+a = 1
+
+while a <= pangkat:
+    if a <= pangkat:
+        print(nilai , end =   " x ")
+        a = a + 1
+    elif a == pangkat:
+        print(nilai , end =   " = ")
+        a = a + 1
+
+sum = nilai * pangkat
+print(sum)
+
+         
+
+    
+        
