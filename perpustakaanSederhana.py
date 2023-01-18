dbdf85a34fcc3a8f4016ac3e10f045d35d602bc7
ALIM RABBANI <100120150+AlimRabbani@users.noreply.github.com>
2023-01-18 18:57:41 +0800

    Add files via upload

---
+++ perpustakaanSederhana.py
@@ -0,0 +1,18 @@
+# Program aplikasi perpustakaan sederhana part 1 
+
+buku = {
+    "Judul" : "Book Dasar - dasar pemrograman",
+    "pengarang" : "zayn Malik",
+    "tahun_terbit" : "1990"
+}
+
+def tampilkan_info_buku(buku):
+    print(f"judul buku   :{buku['Judul']}") 
+    print(f"pengarang    :{buku['pengarang']}") 
+    print(f"Tahun terbit :{buku['tahun_terbit']}")  
+
+tampilkan_info_buku(buku) 
+
+
+
+    
