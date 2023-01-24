21b059cda6031c8e6ffb7eedc2f98398a61b72d3
ALIM RABBANI <100120150+AlimRabbani@users.noreply.github.com>
2023-01-24 16:24:54 +0800

    Add files via upload

---
+++ judul_lagu.py
@@ -0,0 +1,151 @@
+kembali = "y" or "Y"
+while kembali  == "y" or "Y":
+    print("")
+    print("========================================================= ")
+    print("\t\tSELAMAT DATANG DI MEMORY       ")
+    print("=========================================================")
+    print("|=========================================================|")
+    print("\t\tTop 14 Musik viral indonesia\t         ")
+    print("|=========================================================|")
+    print("")
+    print(" 1 . TENTANG CINTA") 
+    print(" 2 . SEMUAH TENTANG KITA ") 
+    print(" 3 . DI ANTARA BINTANG") 
+    print(" 4 . KORBAN JANJI ") 
+    print(" 5 . LINTANG ATI / NITIP ANGIN KANGEN ") 
+    print(" 6 . IBU ") 
+    print(" 7 . PERPISAHAN TERMANIS ") 
+    print(" 8 . BUNDA") 
+    print(" 9 . NANTI ")  
+    print("10 . SEBELUM CAHAYA")  
+    print("11 . DAN ")  
+    print("12 . SEMPURNA ")  
+    print("13 . TAKKAN TERGANTI ")  
+    print("14 . SELAMAT TINGGAL ")  
+    print("")
+
+
+    pilih = int(input("Silahkan Pilih judul lagu sesuai Urutan : ")) 
+
+    if pilih == 1:
+      judul ="TENTANG CINTA" 
+      pencipta_lagu = "ipang"
+      band =  "-----"
+      tahun_rilis ="2007"
+      link_lagu ="https//www.youtubeipang.com" 
+      
+     
+    elif pilih == 2:
+      judul ="SEMUAH TENTANG KITA" 
+      pencipta_lagu = "ARIEL"
+      band = "NOAH"
+      tahun_rilis = 2000
+      link_lagu = "https//www.youtube.com" 
+    elif pilih == 3: 
+      judul =" DI ANTARA BINTANG" 
+      pencipta_lagu = "tidak diketahui"
+      band = "tidak di ketahui"
+      tahun_rilis = 2010
+      link_lagu = "https//www.youtube.com" 
+    elif pilih == 4: 
+      judul =" KORBAN JANJI" 
+      pencipta_lagu = "Faisal Malik Ibrahim"
+      band = "Guyon waton"
+      tahun_rilis = 2018
+      link_lagu = "https//www.youtube.com" 
+    elif pilih == 5: 
+      judul =" LINTANG ATI" 
+      pencipta_lagu = "Genoskun Official"
+      band = "------------"
+      tahun_rilis = 2019
+      link_lagu = "https//www.youtube.com" 
+    elif pilih == 6: 
+      judul =" IBU" 
+      pencipta_lagu = "tidak diketahui"
+      band = "tidak di ketahui"
+      tahun_rilis = 2007
+      link_lagu = "https//www.youtube.com" 
+    elif pilih == 7: 
+      judul =" PERPISAHAN TERMANIS" 
+      pencipta_lagu = "LOVARIAN"
+      band = "LOVARIAN"
+      tahun_rilis = 2007
+      link_lagu = "https//www.youtube.com" 
+    elif pilih == 8: 
+      judul =" BUNDA" 
+      pencipta_lagu = "MELLY GOESLAW"
+      band = "-----"
+      tahun_rilis = 2003
+      link_lagu = "https//www.youtube.com" 
+    elif pilih == 9: 
+      judul =" NANTI" 
+      pencipta_lagu = "Fredy"
+      band = "----"
+      tahun_rilis = 2014
+      link_lagu = "https//www.youtube.com" 
+    elif pilih == 10: 
+      judul =" SEBELUM CAHAYA" 
+      pencipta_lagu = "CAK NUN"
+      band = "LETTO"
+      tahun_rilis = 2000
+      link_lagu = "https//www.youtube.com" 
+    elif pilih == 11: 
+      judul =" DAN" 
+      pencipta_lagu = "SHEILA ON 7"
+      band = "SHEILA ON 7"
+      tahun_rilis = 20006
+      link_lagu = "https//www.youtube.com" 
+    elif pilih == 12: 
+      judul =" SEMPURNA" 
+      pencipta_lagu = "ANDRA AND THE BACKBONE"
+      band = "ANDRA AND THE BACKBONE"
+      tahun_rilis = 1998
+      link_lagu = "https//www.youtube.com" 
+    elif pilih == 13: 
+      judul =" TAKKAN TERGANTI" 
+      pencipta_lagu = "MARCEL SIAHAN"
+      band = "MARCEL SIAHAN"
+      tahun_rilis = 2009
+      link_lagu = "https//www.youtube.com" 
+    elif pilih == 14: 
+      judul ="SELAMAT TINGGAL" 
+      pencipta_lagu = "FIRGOUND FEAT. AUDY"
+      band = "-----"
+      tahun_rilis = 20018
+      link_lagu = "https//www.youtube.com" 
+    else : 
+      
+      print("")
+      kembali=input('lagu tidak tersedia,silahkan masukkan judul lagu yang tersedia ulangi kembali y/n : ') 
+    print("\n=================================================================")
+    print("Judul lagu\t\t :" , judul) 
+    print("Pencipta lagu\t\t : " , pencipta_lagu) 
+    print("band\t\t\t : " , band) 
+    print("Tahun rilis lagu\t : " , tahun_rilis) 
+    print("Link lagu\t\t : " , link_lagu) 
+    print("\n\n=================================================================\n\n")
+    kembali=input('Apakah anda ingin memutar lagu lagi ketik y dan n untuk program selanjutnya  : ') 
+    print("\n\n=================================================================\n\n") 
+    
+
+    # Terimakasih banyak Mr.Hendra 100 harinya :)
+
+    h =0
+    while h <= 100:
+     print("Thank you Mr.Hendra :)",h) 
+     h+=1 
+      
+    
+     
+
+
+    
+
+
+
+
+
+
+
+
+
